bfb92296d328e32d7405bbc94ff1542cbf9ffa86
esam <odoo@eupd2.odoo.com>
2020-01-02 13:29:47 +0000

    new

--- my_module/static/src/js/.ipynb_checkpoints/screens-checkpoint.js
+++ my_module/static/src/js/.ipynb_checkpoints/screens-checkpoint.js
@@ -503,43 +503,6 @@ var NumpadWidget = PosBaseWidget.extend({
    
    
 
-var ActionpadWidget = PosBaseWidget.extend({
-    template: 'ActionpadWidget',
-    init: function(parent, options) {
-        var self = this;
-        this._super(parent, options);
-
-        this.pos.bind('change:selectedClient', function() {
-            self.renderElement();
-        });
-    },
-    renderElement: function() {
-        var self = this;
-        this._super();
-        this.$('.pay').click(function(){
-            var order = self.pos.get_order();
-            var has_valid_product_lot = _.every(order.orderlines.models, function(line){
-                return line.has_valid_product_lot();
-            });
-            //var user = self.pos.get_cashier();
-            var cashier = self.pos.get('cashier') || self.pos.get_cashier();
-       
-            if(cashier){
-                //return user.name;
-              
-                console.log(cashier);        
-                console.log( cashier.name === 'testpos');
-
-            }
-
-
-           
-        });
-        this.$('.set-customer').click(function(){
-            self.gui.show_screen('clientlist');
-        });
-    }
-});
 
 /* --------- The Order Widget --------- */
 
