f244d81ecfefd9e0fc029dac9bdd82508f0fcbd2
esam <odoo@eupd2.odoo.com>
2020-01-02 15:59:45 +0000

    new

--- my_module/static/src/js/.ipynb_checkpoints/screens-checkpoint.js
+++ my_module/static/src/js/.ipynb_checkpoints/screens-checkpoint.js
@@ -7,8 +7,8 @@ var gui = require('point_of_sale.gui');
 
 screens.NumpadWidget.inclue({
     
-    applyAccessRights: function() {
-        console.log("hi i'mmmmmmmmm ");
+        applyAccessRights: function() {
+            console.log("hi i'm 2 ");
         var cashier = this.pos.get('cashier') || this.pos.get_cashier();
         var has_price_control_rights = !this.pos.config.restrict_price_control || cashier.role == 'manager';
         this.$el.find('.mode-button[data-mode="price"]')
@@ -17,7 +17,34 @@ screens.NumpadWidget.inclue({
         if (!has_price_control_rights && this.state.get('mode')=='price'){
             this.state.changeMode('quantity');
         }
-    },
+        if( !cashier.name.includes('cshr') && cashier.role !== 'manager'){
+            //Disable discount on item
+            this.$el.find('.mode-button[data-mode="discount"]')
+            .toggleClass('disabled-mode', true)
+            .prop('disabled', true);
+            //Disable Remove Item from Order
+            this.$el.find('.numpad-backspace')
+            .toggleClass('disabled-mode', true)
+            .prop('disabled', true);
+            //Disable returm
+            this.$('.numpad-minus')
+            .toggleClass('disabled-mode', true)
+            .prop('disabled', true);
+        }else{
+            //Enable discount on item
+            this.$el.find('.mode-button[data-mode="discount"]')
+            .toggleClass('disabled-mode', false)
+            .prop('disabled', false);
+             //Enable Remove Item from Order
+            this.$el.find('.numpad-backspace')
+            .toggleClass('disabled-mode', false)
+            .prop('disabled', false);
+
+             //Enable returm
+             this.$('.numpad-minus')
+             .toggleClass('disabled-mode', false)
+             .prop('disabled', !false);
+        }
        
     },
     
