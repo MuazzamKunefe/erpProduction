a6d9a28c5234395f27b3fa4df02e282dac366e7d
esam <odoo@eupd2.odoo.com>
2020-01-02 13:59:28 +0000

    new

--- my_module/static/src/js/screens.js
+++ my_module/static/src/js/screens.js
@@ -8,27 +8,7 @@ var gui = require('point_of_sale.gui');
 
 
 
-//Custom Code
-var CustomButton = screens.ActionButtonWidget.extend({
-    template: 'screens',
 
-    button_click: function(){
-
-    var self = this;
-    self.custom_function();
-    
-    },
-
-    custom_function: function(){
-		console.log('Hi I am button click of CustomButton');
-    }
-
-});
-
-screens.define_action_button({
-    'name': 'custom_button',
-    'widget': CustomButton,
-});
 
 
 
