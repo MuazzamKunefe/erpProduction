c65b888fa9c2db0a27dcf0624a129c6b05eb7886
esam <odoo@eupd2.odoo.com>
2020-01-07 12:46:19 +0000

    new-time

--- my_module/static/src/js/floors.js
+++ my_module/static/src/js/floors.js
@@ -1,5 +1,28 @@
-odoo.define('custom-time.custom_time', function (require) {    
+odoo.define('custom-time.custom_time', function (require) {
+"use strict";
 
+var floors = require('pos_restaurant.floors');
 
+floors.set_idle_timer.include(
+ set_idle_timer: function(deactivate, timeout) {
+        timeout = timeout || 20000;
+        deactivate = deactivate || false;
+        if (this.idle_timer) {
+            clearTimeout(this.idle_timer);
+        }
+        var self = this;
+        if (deactivate) {
+            clearTimeout(this.idle_timer);
+        } else {
+            this.idle_timer = setTimeout(function(){self.set_table(null)}, timeout);
+        }
+    },
+
+);
     
+
+
+
+
+
 });
