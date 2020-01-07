8e2b31ba4df87c2684a949043b771378dc7883a2
esam <odoo@eupd2.odoo.com>
2020-01-07 12:39:55 +0000

    new-time

--- my_module/static/src/js/.ipynb_checkpoints/floors-checkpoint.js
+++ my_module/static/src/js/.ipynb_checkpoints/floors-checkpoint.js
@@ -1,20 +1,5 @@
 odoo.define('custom-time.custom_time', function (require) {    
 
-set_idle_timer(
-    
-    {
-        timeout = timeout || 2000;
-        deactivate = deactivate || false;
-        if (this.idle_timer) {
-            clearTimeout(this.idle_timer);
-        }
-        var self = this;
-        if (deactivate) {
-            clearTimeout(this.idle_timer);
-        } else {
-            this.idle_timer = setTimeout(function(){self.set_table(null)}, timeout);
-        }
-    },
-            )
+
     
 });
